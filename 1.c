aa16ae494363ecfa387c8bc5505d55a64f995d4f
Algirdas Benetis <44391653+BeneAlgirdas@users.noreply.github.com>
2019-05-16 18:15:20 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -10,7 +10,7 @@ int main()
     //scans string
     scanf("%s", collection);
 
-//string newstring= oldstring.Replace("ì", "").Replace("î", "");
+//string newstring= oldstring.Replace("‚Äú", "").Replace("‚Äù", "");
 
 //    int j = 0;
 //    for (int i = 0; i < 70; i ++) {
@@ -67,61 +67,3 @@ int main()
 
     printf("%d", ourTeamPoints);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//
-//// also works with points that are over 9
-//int calculatePoints(const std::vector<std::string> &results) {
-//	int sum = 0;
-//	for (auto result : results) {
-//		std::string x = "";
-//		std::string y = "";
-//		bool ourTeam = true;
-//		for (int i = 0; i < result.length(); i++) {
-//			if (result[i] == ':') {
-//				ourTeam = false;
-//				continue;
-//			}
-//			if (ourTeam == true) {
-//				x += result[i];
-//			} else {
-//				y += result[i];
-//			}
-//		}
-//		int ourTeamsPoints = std::stoi(x);
-//		int theirTeamsPoints = std::stoi(y);
-//		if (ourTeamsPoints > theirTeamsPoints) {
-//			sum += 3;
-//		} else if (ourTeamsPoints == theirTeamsPoints) {
-//			sum += 1;
-//		}
-//	}
-//	return sum;
-//}
-//
-//int main() {
-//	std::vector< std::string > results = { "3:1", "2:2", "0:1", "3:3", "1:2", "4:2" };
-//	std::cout << calculatePoints(results) << "\n";
-//}
